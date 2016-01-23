2d639bc28048c7cfe0005c3deedce3c5d43bbf7f
Christopher Waldon <christopher.waldon.dev@gmail.com>
2016-01-23 15:01:47 -0500

    Update API to optionally hide disabled controls.

--- shackle.js
+++ shackle.js
@@ -9,25 +9,34 @@ var Shackle = (function(document, window) {
   //Make any change to the state of the enabling
   //control cause the other controls to syncronize
   //their state
-  var pair = function(enableId, controlId) {
+  var pair = function(enableId, controlId, hideControls) {
+    if (hideControls == undefined) {
+      hideControls = false;
+    }
     console.log(enableId, controlId);
     var enable = document.getElementById(enableId);
     console.log(enable);
     var control = document.getElementById(controlId);
     console.log(control);
     enable.addEventListener('click',
-                             _sync.bind(null, enable, control),
+                             _sync.bind(null, enable, control, hideControls),
                              false);
-    _sync(enable, control);
+    _sync(enable, control, hideControls);
   };
   
   //Synchronize the current state of the enabling control
   //to the enabled/disabled control(s)
-  var _sync = function(enable, control) {
+  var _sync = function(enable, control, hideControls) {
     if(enable.checked) {
       control.disabled = false;
+      if (hideControls) {
+        control.hidden = false;
+      }
     } else {
       control.disabled = true;
+      if (hideControls) {
+        control.hidden = true;
+      } 
     }
   };
   return {
